c65cf16259a4e9f43da766333dcad3a957a24278
Stephen <7speter@gmail.com>
2021-10-18 19:52:50 -0400

    Adds initial code to database.js

---
+++ models/database.js
@@ -0,0 +1,57 @@
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+console.log(process.env.DB_HOST)
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack)
+    }
+    client.query('SELECT NOW()', (err, result) => {
+        release()
+        if (err) {
+            return console.error('Error executing query', err.stack)
+        }
+        console.log(result.rows)
+    })
+})
+
+module.exports = {
+    async query(text, params) {
+        const start = Date.now()
+        const res = await pool.query(text, params)
+        const duration = Date.now() - start
+        console.log('executed query', { text, duration, rows: res.rowCount })
+        return res
+    },
+    async getClient() {
+        const client = await pool.connect()
+        const query = client.query
+        const release = client.release
+            // set a timeout of 5 seconds, after which we will log this client's last query
+        const timeout = setTimeout(() => {
+                console.error('A client has been checked out for more than 5 seconds!')
+                console.error(`The last executed query on this client was: ${client.lastQuery}`)
+            }, 5000)
+            // monkey patch the query method to keep track of the last query executed
+        client.query = (...args) => {
+            client.lastQuery = args
+            return query.apply(client, args)
+        }
+        client.release = () => {
+            // clear our timeout
+            clearTimeout(timeout)
+                // set the methods back to their old un-monkey-patched version
+            client.query = query
+            client.release = release
+            return release.apply(client)
+        }
+        return client
+    }
+}
