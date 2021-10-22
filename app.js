a442aa12a42db3d57d971ef24c72762b00d33fc2
Stephen <7speter@gmail.com>
2021-10-21 23:53:22 -0400

    Registers auth route in app.js

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 
 //const db = require('./models/database')
 
+const authRoutes = require('./routes/auth');
+
+app.use(authRoutes);
+
 app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.listen(3002);
