e010649e9ebe92740c7da215d748b2d98b5ee838
Stephen <7speter@gmail.com>
2021-10-24 16:52:43 -0400

    Registers user routes in app.js

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const app = express();
 //const db = require('./models/database')
 
 const authRoutes = require('./routes/auth');
-
+const userRoutes = require('./routes/user');
 app.use(authRoutes);
+app.use(userRoutes);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
